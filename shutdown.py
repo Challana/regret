67aa41d9fb4048debcae6e7ef53da33d1ee5223d
Helga <blairolya@gmail.com>
2021-03-18 22:39:43 +0200

    input validation

--- shutdown.py
+++ shutdown.py
@@ -1,11 +1,20 @@
 import os
-q = input ("Залпнирвоать или отменить")
-if q == "отменить":
-    os.system("shutdown /a")
-elif q == "запланировать":
-    t = input("Введи время (мин): ")
-    t = int(t)
-    t = t*60
-    os.system("shutdown /s /f /t {}".format(t))
-else:
-    print("пиши правильно козел")
+
+flag = True
+while flag:
+    q = input("(З)апланировать или (O)тменить:\n").lower()
+    if q in ("отменить", "о", "запланировать", "з"):
+        flag = False
+    if q == "отменить" or q == "о":
+        os.system("shutdown /a")
+    elif q == "запланировать" or q == "з":
+        t = input("Введи время (мин): ")
+        if not t.isnumeric():
+            print("пиши правильно козел")
+            continue
+        t = int(t)
+        t = t*60
+        os.system("shutdown /a")
+        os.system("shutdown /s /f /t {}".format(t))
+    else:
+        print("пиши правильно козел")
