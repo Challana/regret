db2e1befe8f01c6a574ec9a08c9f005fca6b0305
Helga <blairolya@gmail.com>
2021-03-18 22:28:38 +0200

    add choice

--- shutdown.py
+++ shutdown.py
@@ -1,5 +1,11 @@
 import os
-t = input("введи время")
-t = int(t)
-t = t*60
-os.system("shutdown /s /f /t {}".format(t))
+q = input ("Залпнирвоать или отменить")
+if q == "отменить":
+    os.system("shutdown /a")
+elif q == "запланировать":
+    t = input("Введи время (мин): ")
+    t = int(t)
+    t = t*60
+    os.system("shutdown /s /f /t {}".format(t))
+else:
+    print("пиши правильно козел")
